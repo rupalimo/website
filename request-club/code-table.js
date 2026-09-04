b7627819f02df2d9efdab8fcbed43c3348f2f805
shabana_asthma <36826017+rupalimo@users.noreply.github.com>
2026-09-04 17:15:51 -0400

    add listing and code-table js

---
+++ request-club/code-table.js
@@ -0,0 +1,50 @@
+var SPREADSHEET_ID_AND_TAB =
+	'1l08Sdxv8DRXcq9InKoGzVIIGw-yL3_teI-2C1wcHK6s/posted';
+
+$(document).ready(function () {
+	$.getJSON(
+		'https://opensheet.elk.sh/' + SPREADSHEET_ID_AND_TAB,
+		function (data) {
+			console.log(data);
+
+			data.forEach(function (entry, index) {
+				console.log(entry);
+
+				if (index == 0) return;
+				let requestEntry = $(
+					`<p  data-filter-type="` +
+						entry.date +
+						`">&#91;` +
+						entry.date +
+						`&#93;</p>
+						<p data-filter-type="` +
+						entry.user +
+						`">&#60;` +
+						entry.user +
+						`&#62;</p>
+						<p data-filter-type="` +
+						entry.request +
+						`">` +
+						entry.request +
+						`</p>`,
+				).appendTo('#requestList--index');
+
+				let requestSong = $(
+					`<a target="_blank" href="` +
+						entry.link +
+						`" data-filter-type="` +
+						entry.link +
+						`">` +
+						entry.title +
+						`|` +
+						entry.artist +
+						`|` +
+						entry.year +
+						`|` +
+						entry.album +
+						`</a>`,
+				).appendTo('#requestSong');
+			});
+		},
+	);
+});
